25db17368500c1d678c9f2eb456c5234d0d966a9
CaioAugustoSO <caioaugusto1337@hotmail.com>
2022-10-28 17:21:58 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+ import './screens/categories_screen.dart';
+ import './screens/categores_meals_screen.dart';
+ import './utils/app-routes.dart';
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'DeliMeals',
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
+        accentColor: Colors.pink,
+        fontFamily: 'Raleway',
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        textTheme: ThemeData.light().textTheme.copyWith(
+          titleSmall: TextStyle(
+            fontSize: 20,
+            fontFamily: 'RobotoCondensed',
+          )
+        ),
+      ),
+      routes: {
+        AppRoutes.HOME: (ctx) => Categories(),
+        AppRoutes.CATEGORIES_MEALS: (ctx) => CategoriesMealsScreen(),
+      },
+    );
+  }
+}
